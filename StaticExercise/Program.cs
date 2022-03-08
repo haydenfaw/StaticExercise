b41b83f459cc1cbb71e1052f9b698d11abb25629
Hayden Faw <faw.hayden@gmail.com>
2022-03-08 15:40:43 -0600

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tempC = 7;
+            var tempF = 15;
+
+            Console.WriteLine($"The conversion of {tempC} Celcius to Farenheit is: {TempConverter.CelsiusToFahrenheit(tempC)} Farenheit");
+            Console.WriteLine($"The conversion of {tempF} Farenheit to Celcius is: {TempConverter.FahrenheitToCelsius(tempF)} Celcius");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempF)
+        {
+            double tempC = (tempF - 32) / 1.8;
+            return tempC;
+        }
+        public static double CelsiusToFahrenheit(double tempC)
+        {
+            double tempF = (tempC * 9) / 5 + 32;
+            return tempF;
+        }
+    }
+}
